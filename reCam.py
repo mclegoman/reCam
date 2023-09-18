1b0dfaf78b12f304a3cd77cf7aa404d6ac234264
Daniel-Tayden Larsen <46882277+MCLegoMan@users.noreply.github.com>
2023-09-19 02:50:09 +1200

    Update reCam.py

--- reCam.py
+++ reCam.py
@@ -1,3 +1,8 @@
+# Name: reCam
+# Author: MCLegoMan
+# GitHub: https://github.com/MCLegoMan/reCam
+# License: CC0-1.0
+
 import cv2
 import numpy as np
 import pyaudio
@@ -7,201 +12,202 @@ from tkinter import ttk
 import os
 import datetime
 
-cameraCapture = cv2.VideoCapture(0)
-frame_width = int(cameraCapture.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cameraCapture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+class ReCam:
+    def __init__(self):
+        self.appTitle = "reCam"
+        self.isRunning = False
+        self.isFullscreen = False
+        self.selectedCameraIndex = 0
+        self.selectedMicrophoneIndex = 0
+        self.audioBuffer = []
 
-audio_format = pyaudio.paInt16
-audio_channels = 1
-audio_rate = 44100
-audio_chunk_size = 1024
+        self.frameWidth = 1280
+        self.frameHeight = 720
 
-p = pyaudio.PyAudio()
+        self.cameraCapture = cv2.VideoCapture(self.selectedCameraIndex)
+        self.cameraCapture.set(cv2.CAP_PROP_FRAME_WIDTH, self.frameWidth)
+        self.cameraCapture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frameHeight)
 
-selected_camera_index = 0
-selected_microphone_index = 0
+        self.audioFormat = pyaudio.paInt16
+        self.audioChannels = 1
+        self.audioRate = 44100
+        self.audioChunkSize = 1024
 
-audio_input_stream = None
-audio_output_stream = None
-audio_buffer = []
+        self.p = pyaudio.PyAudio()
 
-video_thread = None
-audio_capture_thread = None
-audio_playback_thread = None
-is_running = True
-is_fullscreen = False
+        self.audioInputStream = None
+        self.audioOutputStream = None
 
-app_title = "reCam"
+        self.videoThread = None
+        self.audioCaptureThread = None
+        self.audioPlaybackThread = None
 
-def set_window_size():
-    cv2.namedWindow(app_title, cv2.WINDOW_NORMAL)
-    cv2.resizeWindow(app_title, frame_width, frame_height)
-    if is_fullscreen:
-        cv2.setWindowProperty(app_title, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-    else:
-        cv2.setWindowProperty(app_title, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
+        self.initGui()
 
-def open_video_window():
-    global is_running, is_fullscreen
-    cv2.namedWindow(app_title, cv2.WINDOW_NORMAL)
-    set_window_size()
+    def initGui(self):
+        self.root = tk.Tk()
+        self.root.title(f"{self.appTitle} - Configuration")
 
-    while is_running:
-        available, frame = cameraCapture.read()
-        if available:
-            cv2.imshow(app_title, frame)
+        self.cameraLabel = ttk.Label(self.root, text="Select Camera:")
+        self.microphoneLabel = ttk.Label(self.root, text="Select Microphone:")
+        self.cameraCombobox = ttk.Combobox(self.root, values=[], state="readonly", width=40)
+        self.microphoneCombobox = ttk.Combobox(self.root, values=[], state="readonly", width=40)
 
-            key = cv2.waitKey(1) & 0xFF
-            if key == 27:
-                is_running = False
+        self.cameraCombobox.bind("<<ComboboxSelected>>", self.setSelectedCamera)
+        self.microphoneCombobox.bind("<<ComboboxSelected>>", self.setSelectedMicrophone)
+
+        self.populateComboboxes()
+
+        self.cameraCombobox.current(self.selectedCameraIndex)
+        self.microphoneCombobox.current(self.selectedMicrophoneIndex)
+
+        self.cameraLabel.grid(row=0, column=0)
+        self.microphoneLabel.grid(row=1, column=0)
+        self.cameraCombobox.grid(row=0, column=1)
+        self.microphoneCombobox.grid(row=1, column=1)
+
+        self.launchButton = ttk.Button(self.root, text="Launch", command=self.launchVideoWindow)
+        self.launchButton.grid(row=2, columnspan=2)
+
+    def populateComboboxes(self):
+        availableCameras = []
+        availableMicrophones = []
+
+        for i in range(10):
+            cap = cv2.VideoCapture(i)
+            if cap.isOpened():
+                availableCameras.append(f"{int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))}x{int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))}")
+                cap.release()
+
+        for i in range(self.p.get_device_count()):
+            deviceInfo = self.p.get_device_info_by_index(i)
+            if deviceInfo['maxInputChannels'] > 0:
+                availableMicrophones.append(deviceInfo['name'])
+
+        self.cameraCombobox['values'] = availableCameras
+        self.microphoneCombobox['values'] = availableMicrophones
+
+    def setSelectedCamera(self, event):
+        self.selectedCameraIndex = self.cameraCombobox.current()
+        self.cameraCapture.release()
+        self.cameraCapture = cv2.VideoCapture(self.selectedCameraIndex)
+        self.cameraCapture.set(cv2.CAP_PROP_FRAME_WIDTH, self.frameWidth)
+        self.cameraCapture.set(cv2.CAP_PROP_FRAME_HEIGHT, self.frameHeight)
+
+    def setSelectedMicrophone(self, event):
+        self.selectedMicrophoneIndex = self.microphoneCombobox.current()
+
+    def setWindowSize(self):
+        cv2.namedWindow(f"{self.appTitle} - Video Output", cv2.WINDOW_NORMAL)
+        cv2.resizeWindow(f"{self.appTitle} - Video Output", self.frameWidth, self.frameHeight)
+        if self.isFullscreen:
+            cv2.setWindowProperty(f"{self.appTitle} - Video Output", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+        else:
+            cv2.setWindowProperty(f"{self.appTitle} - Video Output", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
+
+    def openVideoWindow(self):
+        cv2.namedWindow(f"{self.appTitle} - Video Output", cv2.WINDOW_NORMAL)
+        self.setWindowSize()
+
+        while self.isRunning:
+            available, frame = self.cameraCapture.read()
+            if available:
+                cv2.imshow(f"{self.appTitle} - Video Output", frame)
+
+                key = cv2.waitKey(1) & 0xFF
+                if key == 27:
+                    self.isRunning = False
+                    break
+                elif key == ord('f'):
+                    self.isFullscreen = not self.isFullscreen
+                    if self.isFullscreen:
+                        cv2.setWindowProperty(f"{self.appTitle} - Video Output", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+                    else:
+                        cv2.setWindowProperty(f"{self.appTitle} - Video Output", cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
+                elif key == ord('s'):
+                    self.captureScreenshot(frame, self.isFullscreen)
+
+    def captureScreenshot(self, frame, isFullscreen):
+        scriptDirectory = os.path.dirname(__file__)
+        screenshotFolder = os.path.join(scriptDirectory, "reCamScreenshots")
+
+        try:
+            if not os.path.exists(screenshotFolder):
+                os.makedirs(screenshotFolder)
+        except PermissionError as e:
+            print(f"Permission error: {e}. Unable to create the screenshot folder.")
+            return
+        except Exception as e:
+            print(f"Error: {e}. Unable to create the screenshot folder.")
+            return
+
+        timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+        suffix = 1
+
+        while True:
+            uniqueSuffix = f"{timestamp}_{suffix}"
+            screenshotName = os.path.join(screenshotFolder, f"screenshot_{uniqueSuffix}.png")
+
+            if not os.path.exists(screenshotName):
+                cv2.imwrite(screenshotName, frame)
+
                 break
-            elif key == ord('f'):
-                is_fullscreen = not is_fullscreen
-                if is_fullscreen:
-                    cv2.setWindowProperty(app_title, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
-                else:
-                    cv2.setWindowProperty(app_title, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_NORMAL)
-            elif key == ord('s'):
-                capture_screenshot(frame, is_fullscreen)
 
-def capture_screenshot(frame, is_fullscreen):
-    script_directory = os.path.dirname(__file__)
-    screenshot_folder = os.path.join(script_directory, "reCamScreenshots")
-    
-    try:
-        if not os.path.exists(screenshot_folder):
-            os.makedirs(screenshot_folder)
-    except PermissionError as e:
-        print(f"Permission error: {e}. Unable to create the screenshot folder.")
-        return
-    except Exception as e:
-        print(f"Error: {e}. Unable to create the screenshot folder.")
-        return
-    
-    timestamp = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
-    suffix = 1
+            suffix += 1
 
-    while True:
-        unique_suffix = f"{timestamp}_{suffix}"
-        screenshot_name = os.path.join(screenshot_folder, f"screenshot_{unique_suffix}.png")
+    def captureAudio(self):
+        self.audioInputStream = self.p.open(
+            format=self.audioFormat,
+            channels=self.audioChannels,
+            rate=self.audioRate,
+            input=True,
+            input_device_index=self.selectedMicrophoneIndex,
+            frames_per_buffer=self.audioChunkSize
+        )
 
-        if not os.path.exists(screenshot_name):
-            cv2.imwrite(screenshot_name, frame)
-            
-            break
+        while self.isRunning:
+            audioData = self.audioInputStream.read(self.audioChunkSize)
+            self.audioBuffer.append(np.frombuffer(audioData, dtype=np.int16))
+            if len(self.audioBuffer) > 10:
+                self.audioBuffer.pop(0)
 
-        suffix += 1
+    def playAudio(self):
+        self.audioOutputStream = self.p.open(
+            format=self.audioFormat,
+            channels=self.audioChannels,
+            rate=self.audioRate,
+            output=True
+        )
 
-def capture_audio():
-    global audio_input_stream
-    audio_input_stream = p.open(
-        format=audio_format,
-        channels=audio_channels,
-        rate=audio_rate,
-        input=True,
-        input_device_index=selected_microphone_index,
-        frames_per_buffer=audio_chunk_size
-    )
+        while self.isRunning:
+            if self.audioBuffer:
+                audioData = self.audioBuffer.pop(0).tobytes()
+                self.audioOutputStream.write(audioData)
 
-    while is_running:
-        audio_data = audio_input_stream.read(audio_chunk_size)
-        audio_buffer.append(np.frombuffer(audio_data, dtype=np.int16))
-        if len(audio_buffer) > 10:
-            audio_buffer.pop(0)
+    def videoThreadFunction(self):
+        self.openVideoWindow()
 
-def play_audio():
-    global audio_output_stream
-    audio_output_stream = p.open(
-        format=audio_format,
-        channels=audio_channels,
-        rate=audio_rate,
-        output=True
-    )
+    def audioCaptureThreadFunction(self):
+        self.captureAudio()
 
-    while is_running:
-        if audio_buffer:
-            audio_data = audio_buffer.pop(0).tobytes()
-            audio_output_stream.write(audio_data)
+    def audioPlaybackThreadFunction(self):
+        self.playAudio()
 
-def video_thread_function():
-    open_video_window()
+    def startThreads(self):
+        self.videoThread = threading.Thread(target=self.videoThreadFunction)
+        self.audioCaptureThread = threading.Thread(target=self.audioCaptureThreadFunction)
+        self.audioPlaybackThread = threading.Thread(target=self.audioPlaybackThreadFunction)
 
-def audio_capture_thread_function():
-    capture_audio()
+        self.videoThread.start()
+        self.audioCaptureThread.start()
+        self.audioPlaybackThread.start()
 
-def audio_playback_thread_function():
-    play_audio()
+    def launchVideoWindow(self):
+        self.isRunning = True
+        self.root.destroy()
+        self.startThreads()
+        self.openVideoWindow()
 
-def start_threads():
-    global video_thread, audio_capture_thread, audio_playback_thread
-    video_thread = threading.Thread(target=video_thread_function)
-    audio_capture_thread = threading.Thread(target=audio_capture_thread_function)
-    audio_playback_thread = threading.Thread(target=audio_playback_thread_function)
-
-    video_thread.start()
-    audio_capture_thread.start()
-    audio_playback_thread.start()
-
-root = tk.Tk()
-root.title(f"{app_title} - Configuration")
-
-camera_label = ttk.Label(root, text="Select Camera:")
-microphone_label = ttk.Label(root, text="Select Microphone:")
-camera_combobox = ttk.Combobox(root, values=[], state="readonly", width=40)
-microphone_combobox = ttk.Combobox(root, values=[], state="readonly", width=40)
-
-def set_selected_camera(event):
-    global selected_camera_index, cameraCapture
-    selected_camera_index = camera_combobox.current()
-    cameraCapture.release()
-    cameraCapture = cv2.VideoCapture(selected_camera_index)
-
-def set_selected_microphone(event):
-    global selected_microphone_index
-    selected_microphone_index = microphone_combobox.current()
-
-camera_combobox.bind("<<ComboboxSelected>>", set_selected_camera)
-microphone_combobox.bind("<<ComboboxSelected>>", set_selected_microphone)
-
-def populate_comboboxes():
-    available_cameras = []
-    available_microphones = []
-
-    for i in range(10):
-        cap = cv2.VideoCapture(i)
-        if cap.isOpened():
-            available_cameras.append(f"{int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))}x{int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))}")
-            cap.release()
-
-    for i in range(p.get_device_count()):
-        device_info = p.get_device_info_by_index(i)
-        if device_info['maxInputChannels'] > 0:
-            available_microphones.append(device_info['name'])
-
-    return available_cameras, available_microphones
-
-camera_combobox_values, microphone_combobox_values = populate_comboboxes()
-camera_combobox['values'] = camera_combobox_values
-microphone_combobox['values'] = microphone_combobox_values
-
-selected_camera_index = 0
-selected_microphone_index = 0
-camera_combobox.current(selected_camera_index)
-microphone_combobox.current(selected_microphone_index)
-
-camera_label.grid(row=0, column=0)
-microphone_label.grid(row=1, column=0)
-camera_combobox.grid(row=0, column=1)
-microphone_combobox.grid(row=1, column=1)
-
-def launch_video_window():
-    global is_running
-    is_running = True
-    root.destroy()
-    start_threads()
-    open_video_window()
-
-launch_button = ttk.Button(root, text="Launch", command=launch_video_window)
-launch_button.grid(row=2, columnspan=2)
-
-root.mainloop()
+if __name__ == "__main__":
+    app = ReCam()
+    app.root.mainloop()
